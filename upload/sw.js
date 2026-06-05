de35afd18a73cd2e5f7c8155736f4172171882ed
osugi718 <cap.m0718um@gmail.com>
2026-06-05 14:34:31 +0900

    Update sw.js

--- upload/sw.js
+++ upload/sw.js
@@ -1,8 +1,18 @@
-const CACHE = 'machine-memo-v1';
+const CACHE = 'machine-memo-v3';
 const FILES = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
